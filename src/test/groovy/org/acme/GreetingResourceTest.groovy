33eedfa9fff4664ce8f6e47040bb6a05eef10f74
Robert Kasanicky <robokaso@gmail.com>
2025-03-16 16:00:26 +0100

    configurable greeting

--- src/test/groovy/org/acme/GreetingResourceTest.groovy
+++ src/test/groovy/org/acme/GreetingResourceTest.groovy
@@ -1,6 +1,7 @@
 package org.acme
 
 import io.quarkus.test.junit.QuarkusTest
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.junit.jupiter.api.Test
 
 import static io.restassured.RestAssured.given
@@ -8,13 +9,16 @@ import static org.hamcrest.CoreMatchers.is
 
 @QuarkusTest
 class GreetingResourceTest {
+
+    @ConfigProperty(name = "greeting")
+    String greeting
     @Test
     void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"))
+             .body(is(greeting))
     }
 
 }
